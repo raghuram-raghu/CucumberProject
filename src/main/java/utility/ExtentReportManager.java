8633ae40b3f2046d092da6b4122137b38ed72b8f
raghuram-raghu <raghuramsetty007@gmail.com>
2025-09-10 23:04:58 +0530

    my second commit

---
+++ src/main/java/utility/ExtentReportManager.java
@@ -0,0 +1,39 @@
+package utility;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ExtentReportManager {
+
+    private static ExtentReports extent;
+    private static ExtentTest test;
+
+    public static ExtentReports getReportInstance(){
+
+         if(extent==null) {
+
+             String timestamp = new SimpleDateFormat("dd/MM/yyyy  HH/mm/ss").format(new Date());
+
+             String reportPath = "reports/Extentreport"+timestamp+".html";
+
+             ExtentSparkReporter reporter = new ExtentSparkReporter(reportPath);
+
+             reporter.config().setDocumentTitle("Automation Test Report");
+
+             reporter.config().setReportName("Test Execution Report ");
+
+             extent = new ExtentReports();
+
+             extent.attachReporter(reporter);
+
+         }
+        return extent;
+
+
+    }
+
+}
